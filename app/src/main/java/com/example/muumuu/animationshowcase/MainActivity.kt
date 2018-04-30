f0a8e326a735d43fc2b0e096a5a9b1f682b333d9
atsuko fukui <muumuumuumu@gmail.com>
2018-04-30 19:23:24 +0900

    empty screen

--- app/src/main/java/com/example/muumuu/animationshowcase/MainActivity.kt
+++ app/src/main/java/com/example/muumuu/animationshowcase/MainActivity.kt
@@ -2,19 +2,21 @@ package com.example.muumuu.animationshowcase
 
 import android.os.Bundle
 import android.support.design.widget.NavigationView
+import android.support.v4.content.ContextCompat
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.app_bar_main.*
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+
+        toolbar.title = "ToolbarTest"
+        toolbar.setTitleTextColor(ContextCompat.getColor(this, android.R.color.white))
 
         val toggle = ActionBarDrawerToggle(
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
