79c9670f1bd5fa73d197fde5a5412c3bdf1e4996
Atsuko <atsuko@mercari.com>
2018-07-18 08:48:21 +0900

    update link

--- app/src/main/java/com/example/muumuu/animationshowcase/CodeSampleUrl.kt
+++ app/src/main/java/com/example/muumuu/animationshowcase/CodeSampleUrl.kt
@@ -4,4 +4,5 @@ enum class CodeSampleUrl(val url: String) {
     SCALE_BUTTON("https://github.com/atsuko-fukui/AnimationShowcase/blob/master/app/src/main/java/com/example/muumuu/animationshowcase/ScaleButtonFragment.kt"),
     SHARED_ELEMENT("https://github.com/atsuko-fukui/AnimationShowcase/blob/master/app/src/main/java/com/example/muumuu/animationshowcase/SharedElementFragment.kt"),
     ARC_TRANSITION("https://github.com/atsuko-fukui/AnimationShowcase/blob/master/app/src/main/java/com/example/muumuu/animationshowcase/ArcTransitionFragment.kt"),
+    STATE_LIST_ANIMATION("https://github.com/atsuko-fukui/AnimationShowcase/blob/master/app/src/main/java/com/example/muumuu/animationshowcase/StateListAnimFragment.kt"),
 }
--- app/src/main/java/com/example/muumuu/animationshowcase/StateListAnimFragment.kt
+++ app/src/main/java/com/example/muumuu/animationshowcase/StateListAnimFragment.kt
@@ -1,13 +1,23 @@
 package com.example.muumuu.animationshowcase
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_state_list_animator.viewCodeButton
 
 class StateListAnimFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_state_list_animator, null)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewCodeButton.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(CodeSampleUrl.STATE_LIST_ANIMATION.url)))
+        }
     }
 }
